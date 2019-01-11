946a1baeb44ea32cd17bf7d49fa01c23f9f13f17
周建刚 <zhoujg@weipaitang.com>
2019-01-11 17:37:46 +0800

    add alidsn plugs

--- builder.sh
+++ builder.sh
@@ -29,6 +29,48 @@ if [ "$TELEMETRY" = "false" ]; then
 EOF
 fi
 
+# alidns
+# when pr merge, remove this
+cat /go/src/github.com/mholt/caddy/caddytls/alidns/alidns.go <<EOF
+// alidns
+package alidns
+
+import (
+    "errors"
+
+    "github.com/mholt/caddy/caddytls"
+    "github.com/xenolf/lego/providers/dns/alidns"
+)
+
+func init() {
+    caddytls.RegisterDNSProvider("alidns", NewDNSProvider)
+}
+
+// NewDNSProvider returns a new alidns DNS challenge provider.
+// The credentials are interpreted as follows:
+//
+// len(0): use credentials from environment
+// len(2): credentials[0] = API key
+//         credentials[1] = Secret key
+func NewDNSProvider(credentials ...string) (caddytls.ChallengeProvider, error) {
+    switch len(credentials) {
+    case 0:
+        return alidns.NewDNSProvider()
+    case 2:
+        config := alidns.NewDefaultConfig()
+        config.APIKey = credentials[0]
+        config.SecretKey = credentials[1]
+        return alidns.NewDNSProviderConfig(config)
+    default:
+        return nil, errors.New("invalid credentials length")
+    }
+}
+EOF
+
+cat /go/src/github.com/mholt/caddy/caddytls/alidns.go <<EOF
+import _ "github.com/mholt/caddy/caddytls/alidns"
+EOF
+
 # plugins
 for plugin in $(echo $PLUGINS | tr "," " "); do \
     go get -v $(caddyplug package $plugin); \
